32240e7485f5c9c3d0cda113eb3631c726364508
Andrew Gibson <andrew@andrewresearch.net>
2018-05-01 22:59:40 +1000

    goingok-4 Create basic shell structure to mirror existing goingok

--- goingok_server/src/main/scala/org/goingok/server/views/ProfilePage.scala
+++ goingok_server/src/main/scala/org/goingok/server/views/ProfilePage.scala
@@ -1,34 +1,37 @@
 package org.goingok.server.views
 
-import org.goingok.server.views.fragments.NavBar
-
+import org.goingok.server.views.components.{ComponentTests, NavBar, ProfileContent}
 import scalatags.Text.TypedTag
 
 
 /**
   * Created by andrew@andrewresearch.net on 20/11/17.
   */
-object HomePage extends GenericPage {
+object ProfilePage extends GenericPage {
 
+  import controllers.routes.Assets
   import scalatags.Text.all._
   import scalatags.Text.tags2
-  import controllers.routes.Assets
 
   override def page(titleStr:String):TypedTag[String] = html(
     head(
       tags2.title(titleStr),
       meta(charset:="utf-8"),
       meta(name:="viewport", content:="width=device-width, initial-scale=1"),
+      //meta(attr("http-equiv"):="Content-Security-Policy", content:="script-src https://use.fontawesome.com/releases/v5.0.10/js/all.js"),
       link(rel:="icon",`type`:="image/x-icon", href:=Assets.at("images/favicon.ico").url),
-      link(href:=Assets.at("css/bootstrap.css").url, rel:="stylesheet"),
-      link(href:=Assets.at("css/navbar.component.css").url, rel:="stylesheet"),
+      Includes.fontAwesomeJS,
+      Includes.bootstrapCSS,
+      Includes.goingokNavbarCSS
     ),
     body(
       NavBar.main,
-      ComponentTests.mainContainer,
+      ProfileContent.mainContainer,
+      Includes.jqueryJS,
+      Includes.popperJS,
       Includes.bootstrapJS,
       Includes.d3JS,
-      Includes.goingokJS
+      //Scripts.goingok
     )
   )
 }
