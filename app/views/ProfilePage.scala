c7fcd445a524fe8a63ab86344ee645d708cf221b
Andrew Gibson <andrew@andrewresearch.net>
2019-07-24 11:12:24 +1000

    goingok-20 Fix help tooltips in UI

--- app/views/ProfilePage.scala
+++ app/views/ProfilePage.scala
@@ -30,16 +30,26 @@ object ProfilePage extends GenericPage {
           showMessage(message),
           div( id := "reflectchart-content", `class` := "row",
             div( `class` := "col-sm-12",
-              Includes.panel("reflection-points","fas fa-chart-line","GoingOK over time",ReflectionPointChart.display())
+              Includes.panel("reflection-points","fas fa-chart-line","GoingOK over time",ReflectionPointChart.display(),
+              "Displays how you thought were going in the past. The purple dots are how you thought you were going at any point in time "+
+                "(from distressed to soaring). The red line shows a smoothed path (plotline) of your journey over time, "+
+                "and the yellow line shows your average reflection point.")
             )
           ),
           div( id := "main-content", `class` := "row",
             div( id := "main-left-column", `class` := "col-sm-8",
-              Includes.panel("reflection-entry","fas fa-edit","Enter a reflection",ReflectionEntry.display(sliderStartPoint)),
-              Includes.panel("reflection-list", "fas fa-list-alt", "Past reflections", ReflectionList.display(profile.reflections.map(_.reverse)))
+              Includes.panel("reflection-entry","fas fa-edit","Enter a reflection",ReflectionEntry.display(sliderStartPoint),
+              "Use the slider to indicate how you are going from 'distressed' to 'soaring', "+
+                "and in the text box below write anything you like to describe how you how you are going. "+
+                "When finished, click the 'save' button to record your reflection."),
+              Includes.panel("reflection-list", "fas fa-list-alt", "Past reflections", ReflectionList.display(profile.reflections.map(_.reverse)),
+              "A reverse ordered list of reflections that you have written to date. The number represents your reflection point from 0 to 100 "+
+                "where 0 is 'distressed' and 100 is 'soaring'."
+              )
             ),
             div( id := "main-right-column", `class` := "col-sm-4",
-              Includes.panel("message-list", "fas fa-envelope", "Messages", MessagesList.display())
+              Includes.panel("message-list", "fas fa-envelope", "Messages", MessagesList.display(),
+              "Messages from GoingOK or from your group facilitator will appear here.")
             )
           )
         ),
