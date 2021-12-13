fff1b92480a64753dd009bb0a5d41be13ea4d15c
Miguel Canizares <48811369+maciiv@users.noreply.github.com>
2021-12-13 16:49:28 +1000

    update-sidebar

--- app/views/components/analytics/SidePanel.scala
+++ app/views/components/analytics/SidePanel.scala
@@ -6,10 +6,19 @@ import scalatags.Text.all.{_}
 object SidePanel {
   def display(): TypedTag[String] = {
     div(id:="sidebar", `class`:= "active sticky-top",
-      div(`class`:="sidebar-header",
-        h3("Your groups")
-      ),
-      ul(id:="groups", `class`:="list-unstyled components")
+      div(`class`:="row mr-0",
+        div(`class`:="col-md-10 pr-0",
+          div(`class`:="sidebar-header",
+            h3("Your groups")
+          ),
+          ul(id:="groups", `class`:="list-unstyled components")
+        ),
+        div(`class`:="col-md-2 p-0", id:="analytics-navbar",
+          button(`type`:="button", id:="sibebar-btn", `class`:="btn btn-dark my-4",
+            i(`class`:="fa fa-align-justify")
+          )
+        )
+      )
     )
   }
 }
