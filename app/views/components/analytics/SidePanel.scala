6bac9aa238cc7dd96d71716a200281547e6163ed
maciiv <48811369+maciiv@users.noreply.github.com>
2022-03-04 14:08:13 +1000

    fix bugs

--- app/views/components/analytics/SidePanel.scala
+++ app/views/components/analytics/SidePanel.scala
@@ -6,22 +6,22 @@ import scalatags.Text.all.{_}
 object SidePanel {
   def display(exp: Boolean): TypedTag[String] = {
     div(id:="sidebar", `class`:= "active sticky-top",
-      div(`class`:="row mr-0",
-        div(`class`:="col-md-10 pr-0",
-          div(`class`:="sidebar-header",
-            h3("Your groups")
-          ),
-          ul(id:="groups", `class`:="list-unstyled components"),
-          if (exp) {
-            a(`class`:="btn btn-warning", href:="/analytics", "Switch dashboard")
-          } else {
-            a(`class`:="btn btn-warning", href:="/analytics?exp=true", "Switch dashboard")
-          }
-        ),
-        div(`class`:="col-md-2 p-0", id:="analytics-navbar",
-          button(`type`:="button", id:="sidebar-btn", `class`:="btn btn-dark my-4",
+      div(`class`:="row",
+        div(`class`:="col-md-12 d-flex my-3",
+          h3(`class`:= "mx-auto", "Your groups"),
+          button(`type`:="button", id:="sidebar-btn", `class`:="btn btn-dark mr-2",
             i(`class`:="fa fa-align-justify")
           )
+        ),
+        div(`class`:= "col-md-12",
+          ul(id:="groups", `class`:="list-unstyled components")
+        ),
+        div(id:="switch-dashboard", `class`:="col-md-12 d-flex",
+          if (exp) {
+            a(`class`:="btn btn-warning btn-block m-3", href:="/analytics", "Switch dashboard")
+          } else {
+            a(`class`:="btn btn-warning btn-block m-3", href:="/analytics?exp=true", "Switch dashboard")
+          }
         )
       )
     )
